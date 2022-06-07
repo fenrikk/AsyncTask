dbbdcd11812d03b4f76ba7a37e86a9c4c642969d
Nikita Stukalov <nikita.stukalov07@gmail.com>
2022-06-07 15:46:31 +0300

    upd

--- app/src/main/java/com/nikfen/asynctask/MainActivity.kt
+++ app/src/main/java/com/nikfen/asynctask/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.nikfen.asynctask
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.lifecycleScope
 import com.nikfen.asynctask.databinding.ActivityMainBinding
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
@@ -19,6 +18,8 @@ import kotlin.random.Random
 class MainActivity : AppCompatActivity() {
 
     private val liveData = MutableLiveData<Int>()
+    private var thread: Thread? = null
+    private val compositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,47 +27,50 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.textView.rootView)
 
         val listLiveData = mutableListOf<Int>();
-        Thread {
+        thread = Thread {
             while (!Thread.currentThread().isInterrupted) {
                 Thread.sleep(1000)
                 liveData.postValue(Random.nextInt(0, 100))
             }
-        }.start()
+        }
+        thread!!.start()
         liveData.observe(this) {
             listLiveData.add(it)
             binding.textView.text = "Livedata ${listLiveData.last()}"
         }
 
-        val listRxJava = mutableListOf<Long>()
-        Observable
-            .interval(0, 500, TimeUnit.MILLISECONDS)
+        compositeDisposable.add(Observable
+            .interval(1000L, TimeUnit.MILLISECONDS)
+            .map {
+                Random.nextInt(0, 100)
+            }
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
-                listRxJava.add(it)
-                binding.textView2.text = "RxJava ${listRxJava.last()}"
+                binding.textView2.text = "RxJava $it"
             }, {
                 it.printStackTrace()
             })
+        )
 
         val flow = MutableStateFlow(0)
         lifecycleScope.launch {
-            coroutineScope {
+            while (true) {
+                delay(1000)
                 launch {
-                    while (true) {
-                        flow.value = (Random.nextInt(0, 100))
-                        Log.d("ADA", "onCreate: dd")
-                        delay(500)
-                    }
-                }
-            }
-            coroutineScope {
-                launch {
+                    flow.value = (Random.nextInt(0, 100))
                     flow.collect {
                         binding.textView3.text = "Coroutines $it"
                     }
                 }
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        thread!!.interrupt()
+        thread = null
+        compositeDisposable.dispose()
     }
 }
